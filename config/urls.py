ab4fa832c98203d380e0295a3a68e7340e0b7ab0
1f10230237 <s1f102302371@iniad.org>
2024-07-03 22:57:35 +0900

    トップページを追加

--- config/urls.py
+++ config/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from todoo import views as todoo_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', todoo_views.index, name='index'),
 ]
