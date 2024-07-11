436390c1127894a5f5373a10a9f6addc29bf22ce
s1f10230237Superkiiroman <s1f102302371@iniad.org>
2024-07-11 12:14:03 +0900

    Update views.py

--- todo/views.py
+++ todo/views.py
@@ -32,14 +32,18 @@ def detail(request, task_id):
     return render(request, 'todo/detail.html', context)
 
 def close(request, task_id):
-def delete(request, task_id):
-  
     try:
         task = Task.objects.get(pk=task_id)
     except Task.DoesNotExist:
         raise Http404("Task does not exist")
-        
     task.completed = True
     task.save()
+
+def delete(request, task_id):
+    try:
+        task = Task.objects.get(pk=task_id)
+    except Task.DoesNotExist:
+        raise Http404("Task does not exist")
+    
     task.delete()
     return redirect(index)
